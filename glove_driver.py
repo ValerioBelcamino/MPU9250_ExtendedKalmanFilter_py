c9fc6a84a5a4801a78ebe4b17ab177eaf46d78dc
Valerio Belcamino <ValerioBelcamino@gmail.com>
2023-05-26 15:03:29 +0200

    ip changes

--- glove_driver.py
+++ glove_driver.py
@@ -10,13 +10,9 @@ from IMU_class import IMU, Quat_array, Acc_array, Gyro_array, Mag_array
 '''LA CLASSE CHE GESTISCE LA COMUNICAZIONE SOCKET E LA RICEZIONE DEI DATI'''
 class ImuDriver:
 
-    def __init__(self, udp_port):
-        # print(json.loads(requests.get("https://ip.seeip.org/jsonip?").text)["ip"])
-        # ipaddr = json.loads(requests.get("https://ip.seeip.org/jsonip?").text)["ip"]
-        ipaddr = '130.251.13.107'
-        self.udp_ip = ipaddr #'130.251.13.158' #STATIC IP ETHERNET CABLE; '192.168.0.101' #Hard-coded static IP
+    def __init__(self, udp_port, ip_addr):
+        self.udp_ip = ip_addr
         self.udp_port = udp_port
-        # print(ipaddr)
         self.counter = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
         with open("config/driver_config.txt") as f:
             lines = f.readlines()
@@ -26,20 +22,15 @@ class ImuDriver:
             values = line.strip().split(" ")
             self.names[int(values[0])] = values[1]
 
-        # print(sorted([n for n in self.names.values()]))
         self.namelist = sorted([n for n in self.names.values() if 'wrist' not in n])
         self.connectedIMUs = {n:0 for n in self.namelist}
-        # print(self.connectedIMUs)
         self.msgCounter = 0
 
-
-        '''CREAZIONE E CONNESSIONE AL SOCKET'''
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind((self.udp_ip, self.udp_port))
         sock.settimeout(2)
 
         while True:
-            '''LETTURA DA SOCKET DI UN PACCHETTO DI DIMENSIONE 29+6 BYTE'''
             try:
                 data = sock.recv(29+6)  # (29) # buffer size is 1024 bytes
             except Exception as e:
@@ -48,14 +39,10 @@ class ImuDriver:
                 print("Socket closed, returning...")
                 exit()
             if not data:
-                '''MESSAGGIO VUOTO'''
                 print("no data")
             else:
 
-                ''' IL MESSAGGIO NON È VUOTO E PROCEDIAMO A SPACCHETTARLO'''
                 ID = data[28+6]
-                # Fix to ensure topic is published under same name with both devices
-                # print('ID: ', ID, ' \t - \t', self.names[ID])
                 mux = int(ID/100)
                 channel = int((ID-mux*100)/10)
                 address = int(ID-mux*100-channel*10)
@@ -89,20 +76,13 @@ class ImuDriver:
                         gyro,
                         mag)
 
-                '''QUESTA VARIABILE CONTIENE IL NOME DEL SENSORE E VA USATO PER SMISTARE I MESSAGGI SU FILE DIVERSI (IDEALMENTE VOGLIAMO UNA CARTELLA /recordings AL CUI
-                    INTERNO VENGANO CREATI 11 FILE DIVERSI, UNO PER OGNI SENSORE'''
                 sensor_name = self.names[ID] + "-Pose"
 
-                '''TMP CONTIENE UNA STRINGA CON TIMESTAMP, ORIENTAZIONE (X,Y,Z,W), ACCELERAZIONI (X,Y,Z) E MAGNETOMETRO (X,Y,Z) SEPARATE DA VIRGOLA'''
                 tmp = (str(imu.time) + "," + 
                     str(imu.orientation.x) + "," + str(imu.orientation.y) + "," + str(imu.orientation.z) + "," + str(imu.orientation.w) + "," + 
                     str(imu.accelerometer.x) + "," + str(imu.accelerometer.y) + "," + str(imu.accelerometer.z) + "," +
                     str(imu.gyroscope.x) + "," + str(imu.gyroscope.y) + "," + str(imu.gyroscope.z) + "," +
                     str(imu.magnetometer.x) + "," + str(imu.magnetometer.y) + "," + str(imu.magnetometer.z) + "\n")
-
-                '''QUI BISOGNA INSERIRE LA STAMPA SU FILE DELLA STRINGA TMP CHE CONTIENE TUTTE LE INFORMAZIONI CHE CI SERVONO'''
-                
-                #//////////////////////////////////////
 
                 folderpath = "C:\\Users\\valer\\OneDrive\\Desktop\\KalmanFilter\\Magnetometer_recordings"#"C:/recordings"
                 try:
@@ -131,7 +111,7 @@ class ImuDriver:
 
 '''QUI SI ISTANZIA IL NODO ROS CHE NON CI SERVE PIÙ E SI LEGGONO I PARAMETRI PASSATI DA LINEA DI COMANDO (X ES. LA PORTA SU CUI APRIRE IL SOCKET)'''
 def main(): 
-    imu_driver = ImuDriver(2390)
+    imu_driver = ImuDriver(2390, '130.251.13.107')
     imu_driver.listener()
 
 
