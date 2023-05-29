b3c624e768f5504834f84231162f07b8330248d3
Valerio Belcamino <ValerioBelcamino@gmail.com>
2023-05-29 15:25:34 +0200

    magnetometer heading + kalman intro

--- glove_driver.py
+++ glove_driver.py
@@ -5,14 +5,84 @@ import argparse
 import os
 import json, requests
 from IMU_class import IMU, Quat_array, Acc_array, Gyro_array, Mag_array
+import time
+import numpy as np
+import math
+import quaternionic
+from filterpy.kalman import ExtendedKalmanFilter
 
+
+def computeRPY(acc, mag):
+    roll = math.atan2(acc[1], acc[2])
+    pitch = math.atan2(-acc[0], math.sqrt(acc[1]*acc[1] + acc[2]*acc[2]))
+    yaw = math.atan2(mag[2]*math.sin(pitch)-mag[1]*math.cos(pitch), 
+    mag[0]*math.cos(roll)+mag[1]*math.sin(roll)*math.sin(pitch)*mag[2]*math.sin(roll)*math.cos(pitch))
+
+    # math.atan2(coorectedMag[2]*math.sin(pitch)-coorectedMag[1]*math.cos(pitch),
+    #  coorectedMag[0]*math.cos(roll)+coorectedMag[1]*math.sin(roll)*math.sin(pitch)*coorectedMag[2]*math.sin(roll)*math.cos(pitch))
+    return roll, pitch, yaw
+
+def F(quat, gyro, bias):
+    g = gyro
+    b = bias
+    q = quat
+
+    uno = -0.5*[ (g[0]-b[0])*q[1] + (g[1]-b[1])*q[2] + (g[2]-b[2])*q[3]]
+    due = -0.5*[ (g[0]-b[0])*q[0] + (g[1]-b[1])*q[3] + (g[2]-b[2])*q[2]]
+    tre = -0.5*[ (g[0]-b[0])*q[3] + (g[1]-b[1])*q[0] + (g[2]-b[2])*q[1]]
+    quattro = -0.5*[ (g[0]-b[0])*q[2] + (g[1]-b[1])*q[1] + (g[2]-b[2])*q[0]]
+
+    return np.array([uno, due, tre, quattro, 0, 0, 0])
+
+def H(quat):
+    q = quat
+
+    dTheta_dq0 = (2*q[3]^2*q[1] - 4*q[3]*q[0]*q[2] - 2*q[1]*(x^2 + q[1]^2 + q[2]^2))/(q[3]^4 + x^4 + 8*q[3]*q[0]*q[1]*q[2] + 2*x^2*(q[1]^2 - q[2]^2) + 2*q[3]^2*(q[0]^2 - q[1]^2 + q[2]^2) + (q[1]^2 + q[2]^2)^2)
+    dTheta_dq1 = (2*(q[3]^2*q[0] + 2*q[3]*q[1]*q[2] + q[0]*(q[0]^2 + q[1]^2 - q[2]^2)))/(q[3]^4 + q[0]^4 + 8*q[3]*q[0]*q[1]*q[2] + 2*q[0]^2 (q[1]^2 - q[2]^2) + 2*q[3]^2*(q[0]^2 - q[1]^2 + q[2]^2) + (q[1]^2 + q[2]^2)^2)
+    dTheta_dq2 = (2*(q[3]^3 + 2*q[0]*q[1]*q[2] + q[3]*(q[0]^2 - q[1]^2 + q[2]^2)))/(q[3]^4 + q[0]^4 + 8*q[3]*q[0]*q[1]*q[2] + 2*q[0]^2*(q[1]^2 - q[2]^2) + 2*q[3]^2*(q[0]^2 - q[1]^2 + q[2]^2) + (q[1]^2 + q[2]^2)^2)
+    dTheta_dq3 = (-2*(2*q[3]*q[0]*q[1] + q[3]^2*q[2] + q[2]*(-q[0]^2 + q[1]^2 + q[2]^2)))/(q[3]^4 + q[0]^4 + 8*q[3]*q[0]*q[1]*q[2] + 2*q[0]^2 (q[1]^2 - q[2]^2) + 2*q[3]^2*(q[0]^2 - q[1]^2 + q[2]^2) + (q[1]^2 + q[2]^2)^2)
+
+    dPhi_dq0 = (2*q[2])/math.sqrt[1 - 4 (q[3]*q[1] - q[0]*q[2])^2]
+    dPhi_dq1 = (-2*q[3])/math.sqrt[1 - 4 (q[3]*q[1] - q[0]*q[2])^2]
+    dPhi_dq2 = (2*q[0])/math.sqrt[1 - 4 (q[3]*q[1] - q[0]*q[2])^2]
+    dPhi_dq3 = (-2*q[1])/math.sqrt[1 - 4 (q[3]*q[1] - q[0]*q[2])^2]
+
+    dTheta_dq0 = (-2*(q[3]^3 + 2*q[0]*q[1]*q[2] + q[3]*(q[0]^2 - q[1]^2 + q[2]^2)))/(q[3]^4 + q[0]^4 + 8*q[3]*q[0]*q[1]*q[2] + 2*q[0]^2*(q[1]^2 - q[2]^2) + 2*q[3]^2*(q[0]^2 - q[1]^2 + q[2]^2) + (q[1]^2 + q[2]^2)^2)
+    dTheta_dq1 = (-2*(2*q[3]*q[0]*q[1] + q[3]^2*q[2] + q[2]*(-q[0]^2 + q[1]^2 + q[2]^2)))/(q[3]^4 + q[0]^4 + 8*q[3]*q[0]*q[1]*q[2] + 2*q[0]^2*(q[1]^2 - q[2]^2) + 2*q[3]^2*(q[0]^2 - q[1]^2 + q[2]^2) + (q[1]^2 + q[2]^2)^2)
+    dTheta_dq2 = (-2*q[3]^2*q[1] + 4*q[3]*q[0]*q[2] + 2*q[1]*(q[0]^2 + q[1]^2 + q[2]^2))/(q[3]^4 + q[0]^4 + 8*q[3]*q[0]*q[1]*q[2] + 2*q[0]^2*(q[1]^2 - q[2]^2) + 2*q[3]^2*(q[0]^2 - q[1]^2 + q[2]^2) + (q[1]^2 + q[2]^2)^2)
+    dTheta_dq3 = (2*(q[3]^2*q[0] + 2*q[3]*q[1]*q[2] + q[0]*(q[0]^2 + q[1]^2 - q[2]^2)))/(q[3]^4 + q[0]^4 + 8*q[3]*q[0]*q[1]*q[2] + 2*q[0]^2*(q[1]^2 - q[2]^2) + 2*q[3]^2*(q[0]^2 - q[1]^2 + q[2]^2) + (q[1]^2 + q[2]^2)^2)
+
+    return np.array([[dTheta_dq0, dTheta_dq1, dTheta_dq2, dTheta_dq3,0,0,0],
+                    [dPhi_dq0, dPhi_dq1, dPhi_dq2, dPhi_dq3,0,0,0], 
+                    [dPsi_dq0, dPsi_dq1, dPsi_dq2, dPsi_dq3,0,0,0]])
+
+def mag2acc_frame(vector3):
+    vector3=vector3.reshape(3,1)
+    result = np.matrix([[0, 1, 0], [1, 0, 0], [0, 0, -1]]) @ vector3
+    return np.array([result[0,0], result[1,0], result[2,0]])
+
+def correctMagnetometer(vector3):
+    b = np.array([-12.252622, 53.44271, -60.0742])
+    A_1 = np.array([[1.5846435, 0.028956, -0.015616], [0.028956, 1.5994587, -0.01484], [-0.01561694, -0.0148454, 1.5684942]])
+    # subtract the hard iron offset
+    xm_off = vector3[0]-b[0]
+    ym_off  = vector3[1]-b[1]
+    zm_off  = vector3[2]-b[2]
+    
+    #multiply by the inverse soft iron offset
+    xm_cal = xm_off *  A_1[0,0] + ym_off *  A_1[0,1]  + zm_off *  A_1[0,2] 
+    ym_cal = xm_off *  A_1[1,0] + ym_off *  A_1[1,1]  + zm_off *  A_1[1,2] 
+    zm_cal = xm_off *  A_1[2,0] + ym_off *  A_1[2,1]  + zm_off *  A_1[2,2] 
+
+    return np.asarray([xm_cal, ym_cal, zm_cal])
 
 '''LA CLASSE CHE GESTISCE LA COMUNICAZIONE SOCKET E LA RICEZIONE DEI DATI'''
 class ImuDriver:
 
-    def __init__(self, udp_port, ip_addr):
+    def __init__(self, udp_port, ip_addr, recording=False):
         self.udp_ip = ip_addr
         self.udp_port = udp_port
+        self.recording = recording
         self.counter = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
         with open("config/driver_config.txt") as f:
             lines = f.readlines()
@@ -28,7 +98,7 @@ class ImuDriver:
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind((self.udp_ip, self.udp_port))
-        sock.settimeout(2)
+        sock.settimeout(10)
 
         while True:
             try:
@@ -68,7 +138,7 @@ class ImuDriver:
                     struct.unpack('<h', data[30:32])[0],
                     struct.unpack('<h', data[32:34])[0])
 
-                imu = IMU(Time.time(),
+                imu = IMU(time.time(),
                         self.names[ID],
                         ID,
                         quat,
@@ -76,7 +146,7 @@ class ImuDriver:
                         gyro,
                         mag)
 
-                sensor_name = self.names[ID] + "-Pose"
+                sensor_name = self.names[ID]
 
                 tmp = (str(imu.time) + "," + 
                     str(imu.orientation.x) + "," + str(imu.orientation.y) + "," + str(imu.orientation.z) + "," + str(imu.orientation.w) + "," + 
@@ -90,28 +160,39 @@ class ImuDriver:
                 except FileExistsError:
                     pass
 
-                filename = os.path.join(folderpath, sensor_name+".txt")  
-                f = open(filename, "a+")
-                f.write(tmp)
-                f.close()
-
-                #/////////////////////////////////////
-
+                if recording:
+                    filename = os.path.join(folderpath, sensor_name+".txt")  
+                    f = open(filename, "a+")
+                    f.write(tmp)
+                    f.close()
                 
-                self.connectedIMUs[self.names[ID]] = 1
-                self.msgCounter += 1
-                if self.msgCounter == 100:
-                    self.connectedIMUs = {n:0 for n in self.namelist}
-                    self.msgCounter = 0
-                elif  self.msgCounter > 30:
-                    print(self.connectedIMUs, end='\r')
-                '''QUESTA È LA PARTE CHE COMUNICAVA CON ROS E NON CI SERVE PIÙ'''
+                # self.connectedIMUs[self.names[ID]] = 1
+                # self.msgCounter += 1
+                # if self.msgCounter == 100:
+                #     self.connectedIMUs = {n:0 for n in self.namelist}
+                #     self.msgCounter = 0
+                # elif  self.msgCounter > 30:
+                #     print(self.connectedIMUs, end='\r')
 
+
+                mag = np.array([imu.magnetometer.x, imu.magnetometer.y, imu.magnetometer.z])
+                acc = np.array([imu.accelerometer.x, imu.accelerometer.y, imu.accelerometer.z])
+                correctedMag = mag2acc_frame( correctMagnetometer(mag))
+
+                roll, pitch, yaw = computeRPY(acc, correctedMag)
+
+                # roll = roll * 180 / math.pi
+                # pitch = pitch * 180 / math.pi
+                # yaw = yaw * 180 / math.pi
+                # print(f"roll: {roll:0.3f}, pitch: {pitch:0.3f}, yaw: {yaw:0.3f}")
+
+                quat = quaternionic.array.from_euler_angles(roll, pitch, yaw)
+                print(f"quat: {quat.w:.2f}, {quat.x:.2f}, {quat.y:.2f}, {quat.z:.2f}")
 
 
 '''QUI SI ISTANZIA IL NODO ROS CHE NON CI SERVE PIÙ E SI LEGGONO I PARAMETRI PASSATI DA LINEA DI COMANDO (X ES. LA PORTA SU CUI APRIRE IL SOCKET)'''
 def main(): 
-    imu_driver = ImuDriver(2390, '130.251.13.107')
+    imu_driver = ImuDriver(2390, '130.251.13.116', recording=False)
     imu_driver.listener()
 
 
